0071be146127c1ba434873e0f560268f8dbd981c
tanxok <limbualex2@gmail.com>
2023-04-04 14:54:56 +0545

    removed imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from './components/Home'
 import Contact from './components/Contact'
-import Payment from './components/Payment'
-import Vehicle from './components/Vehicle'
-import Renting from './components/Renting'
 
 
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,26 +5,8 @@ import Nav from './Nav'
 const Home = () => {
   return (
     <div class="home">
-    <div>
-        <Nav/>
-    </div>
-    <div>
-        <h1>BIKE RENTAl</h1>
-        <h2>Welcome to our bike rental service, where we offer a wide selection of quality bikes to help you explore your surroundings and stay active. 
-          Whether you're a tourist looking to sightsee, a commuter seeking a greener mode of transportation, or an avid cyclist in need of a reliable ride, we've got you covered. 
-          Our rental fleet includes various styles and sizes of bicycles, from mountain bikes to city cruisers, all at affordable rates. We also provide safety equipment, such as helmets and locks, to ensure a worry-free and enjoyable biking experience. 
-          Book online or visit our shop today to start your adventure!
-
-        </h2>
-
-
-    </div>
-    
-    <div>
-      <h1>our services</h1>
-    </div>
-    
-    
+      <Nav />
+      Home
     </div>
 
   );
